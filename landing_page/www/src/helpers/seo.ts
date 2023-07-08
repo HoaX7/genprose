4f0bca5a57a1c50b3b71447e7114a9e3468fafb5
hoax <kvnrivers@gmail.com>
2023-07-08 13:26:45 +0530

    improved seo content & issue fixes

--- landing_page/www/src/helpers/seo.ts
+++ landing_page/www/src/helpers/seo.ts
@@ -1,12 +1,12 @@
 export const SEO = {
     home: {
-        title: "Gen Prose Early Access",
-        description: "Get early access to GenProse, the AI-powered content generator. Convert audio, video and podcasts links to generate SEO-optimized blog articles.",
-        keywords: "genprose auxiliary content, free audio to text converter, podcast to text, ai seo content generator, genprose, seo blog articles, youtube video to text generator, audio to text converter, content writing services, generative ai in seconds"
+        title: "Gen Prose, Converting audio to text",
+        description: "Get early access to GenProse, the AI-powered content generator. Convert audio, video, podcasts & webinar links to SEO-optimized blog articles.",
+        keywords: "convert audio to text, podcast to text, ai seo content generator, genprose, seo blog articles, youtube video to text generator, audio to text converter free online, content writing services"
     },
     articles: {
-        title: "Gen Prose Articles",
+        title: "Gen Prose, Useful Articles",
         description: "Discover informative articles about the YouTube video to text generator. Learn to transcribe videos effortlessly and enhance your content creation process.",
-        keywords: "genprose articles, genprose blogs, free audio to text converted, podcast to text blogs, about genprose, articles about youtube video to text generator"
+        keywords: "content optimization, transcribe audio to text free, free audio to text converter, podcast to text blogs, audio transcriber, articles about youtube video to text generator, how to use genprose, free online audio transcriber"
     }
 }
